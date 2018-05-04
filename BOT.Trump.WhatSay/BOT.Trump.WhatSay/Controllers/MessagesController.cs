1b8cea038c19bd73e6dcef87e16227e9d6bc9009
Gagan Jeet Singh <GaganS@intergen.org.nz>
2018-05-04 15:40:41 +1200

    BOT for what Trump said

---
+++ BOT.Trump.WhatSay/BOT.Trump.WhatSay/Controllers/MessagesController.cs
@@ -0,0 +1,68 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System;
+
+namespace BOT.Trump.WhatSay
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
+            }
+            else
+            {
+                HandleSystemMessageAsync(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private async Task<Activity> HandleSystemMessageAsync(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                ConnectorClient client = new ConnectorClient(new Uri(message.ServiceUrl));
+                foreach (var newMember in message.MembersAdded)
+                {
+                    if (newMember.Id != message.Recipient.Id)
+                    {
+                        var reply = message.CreateReply();
+                        reply.Text = "Ask me anything!!! Go Crazy";
+                        await client.Conversations.ReplyToActivityAsync(reply);
+                    }
+                }
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
---
+++ BOT.Trump.WhatSay/BOT.Trump.WhatSay/Dialogs/RootDialog.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace BOT.Trump.WhatSay.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            SendWelcomeMessage(context);
+        }
+
+        /// <summary>
+        /// welcome message on load
+        /// </summary>
+        /// <param name="context">User contect</param>
+        /// <returns>User message</returns>
+        private void SendWelcomeMessage(IDialogContext context)
+        {
+            context.Call(new NameDialog(), this.ResumeAfterNameDialog);
+        }
+
+        /// <summary>
+        /// Get the name and start the conversation
+        /// </summary>
+        /// <param name="context">User context</param>
+        /// <param name="result">User result</param>
+        /// <returns>End of Conversation</returns>
+        private async Task ResumeAfterNameDialog(IDialogContext context, IAwaitable<object> result)
+        {
+            try
+            {
+                var activity = await result as Activity;
+
+                // Get the tweets
+                /* Add function here 
+                 * */
+
+                // return our reply to the user
+                await context.PostAsync($"");
+
+                context.Wait(ResumeAfterNameDialog);
+            }
+            catch (Exception ex)
+            {
+                await context.PostAsync($"Failed with message: {ex.Message}");
+                context.Done(EndOfConversationCodes.ChannelFailed);
+            }
+        }
+    }
+}
